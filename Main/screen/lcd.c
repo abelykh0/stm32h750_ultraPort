9be7219b22bfebbb3fefd611de8267a4cee16f16
Andrey Belykh <abelykh@yahoo.ca>
2025-03-17 23:58:06 -0400

    Update lcd.c

--- Main/screen/lcd.c
+++ Main/screen/lcd.c
@@ -35,6 +35,7 @@ static void SPI_WriteData(uint16_t data)
     LCD_SPI_CS(1);
 }
 
+// ILI9481
 void lcd_init(void)
 {
     LCD_SPI_CS(1);
@@ -44,13 +45,13 @@ void lcd_init(void)
 
     SPI_WriteComm(0x11); // exit_sleep_mode
     LCD_delay(130);
-    SPI_WriteComm(0x13);
+    SPI_WriteComm(0x13); // Enter_normal_mode
     LCD_delay(120);
 
     SPI_WriteComm(0x11);
     LCD_delay(20);
-    SPI_WriteComm(0x3a); // set_pixel_format
-    SPI_WriteData(0x55); // 18bit/pixel
+    SPI_WriteComm(0x3a); // Set_pixel_format
+    SPI_WriteData(0x55); // 16 bits/pixel
 
     SPI_WriteComm(0xD0); // Power Setting
     SPI_WriteData(0x07);
@@ -62,11 +63,11 @@ void lcd_init(void)
     SPI_WriteData(0x14);
     SPI_WriteData(0x1B);
 
-    SPI_WriteComm(0xD2);
+    SPI_WriteComm(0xD2); // Power setting for Normal Mode
     SPI_WriteData(0x01);
     SPI_WriteData(0x12);
 
-    SPI_WriteComm(0xC0);
+    SPI_WriteComm(0xC0); // Panel Driving Setting
     SPI_WriteData(0x00);
     SPI_WriteData(0x3B);
     SPI_WriteData(0x00);
@@ -74,12 +75,12 @@ void lcd_init(void)
     SPI_WriteData(0x01);
 
     SPI_WriteComm(0xC5); // Frame rate and Inversion Control
-    SPI_WriteData(0x04); // Frame rate:0x04 56hz
+    SPI_WriteData(0x02); // Frame rate : 0x02 85 Hz
 
     SPI_WriteComm(0xC6); // Interface Control
     SPI_WriteData(0x02); // SDA_EN(D7) = 0, EPL(D1) = 0, DPL(D0) = 0
 
-    SPI_WriteComm(0xC8);
+    SPI_WriteComm(0xC8); // Gamma setting
     SPI_WriteData(0x00);
     SPI_WriteData(0x46);
     SPI_WriteData(0x44);
@@ -93,31 +94,31 @@ void lcd_init(void)
     SPI_WriteData(0x0F);
     SPI_WriteData(0x00);
 
-    SPI_WriteComm(0x36);
+    SPI_WriteComm(0x36); // Set Address Mode
     SPI_WriteData(0x48);
     SPI_WriteComm(0x3A);
     SPI_WriteData(0x66);
-
-    SPI_WriteComm(0x2A);
+/*
+    SPI_WriteComm(0x2A); // Set Column Address
     SPI_WriteData(0x00);
     SPI_WriteData(0x00);
     SPI_WriteData(0x01);
     SPI_WriteData(0x3F);
 
-    SPI_WriteComm(0x2B);
+    SPI_WriteComm(0x2B); // Set Page Address
     SPI_WriteData(0x00);
     SPI_WriteData(0x00);
     SPI_WriteData(0x01);
     SPI_WriteData(0xdf);
-
+*/
     LCD_delay(120);
-    SPI_WriteComm(0x29);
+    SPI_WriteComm(0x29); // Set Display On
 
-    SPI_WriteComm(0x2C); // Write_memory_start
+    //SPI_WriteComm(0x2C); // Write_memory_start
 
     LCD_delay(10);
-    SPI_WriteComm(0x20);
+    SPI_WriteComm(0x20); // Exit Invert Mode
 
-    SPI_WriteComm(0xB4);
+    SPI_WriteComm(0xB4); // Display and Frame Memory Write Mode
     SPI_WriteData(0x10);
 }
